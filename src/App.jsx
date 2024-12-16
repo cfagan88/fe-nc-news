faf9d58325b4fc079e7226f20a679387fd75addd
Chris Fagan <cfagan88@hotmail.co.uk>
2024-12-16 14:19:20 +0000

    adds homepage with all articles rendered

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
 import './App.css'
+import { Route, Routes } from "react-router";
+import Header from './components/Header'
+import ArticlesList from './components/ArticlesList'
+import Footer from './components/Footer'
 
 function App() {
   return (
     <>
+    <Header/>
+    <Routes>
+      <Route path="/" element={<ArticlesList />}></Route>
+    </Routes>
+    <Footer/>
     </>
   )
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+function Header() {
+  return (
+    <>
+      <h1 className="header">NC News</h1>
+      <nav>
+        <p className="nav-bar">Home</p>
+        <p className="nav-bar">Topics</p>
+      </nav>
+    </>
+  );
+}
+
+export default Header;
