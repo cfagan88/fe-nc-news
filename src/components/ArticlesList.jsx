67c0b3e130b00128eb02ce58db50bed4ce23883b
Chris Fagan <cfagan88@hotmail.co.uk>
2024-12-17 10:28:58 +0000

    adds individual article page

--- src/components/ArticlesList.jsx
+++ src/components/ArticlesList.jsx
@@ -1,23 +1,12 @@
-import { useEffect, useState } from "react";
-import { fetchArticles } from "../api";
-import ArticleCards from "./ArticleCards";
+import ArticleCard from "./ArticleCard";
 
-function ArticlesList() {
-  // Set state as context for articles
-  const [articles, setArticles] = useState([]);
-
-  useEffect(() => {
-    fetchArticles().then((articles) => {
-      setArticles(articles);
-    });
-  }, []);
-
+function ArticlesList({articles}) {
   return (
     <>
       <h2>Latest Articles</h2>
       <ul className="article-list">
         {articles.map((article) => {
-          return <ArticleCards key={article.article_id} article={article} />;
+          return <ArticleCard key={article.article_id} article={article} />;
         })}
       </ul>
     </>
